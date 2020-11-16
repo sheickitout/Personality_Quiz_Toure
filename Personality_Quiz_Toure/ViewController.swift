777b6c1e5eae91d74c78633bd8d50e54eb350127
rojoMojoGit <sheickitout@gmail.com>
2020-11-16 03:59:54 -0500

    Initial Commit

---
+++ Personality_Quiz_Toure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personality_Quiz_Toure
+//
+//  Created by Sheick on 11/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
